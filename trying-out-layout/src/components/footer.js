f29667835e9f19e18708b83268566d86f8f6bcdd
Johan van den Heuvel <johanheuvel5@gmail.com>
2019-01-22 13:40:24 +0100

    second commit

---
+++ trying-out-layout/src/components/footer.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+export default () => (
+	<div style={{ background:"blue"}}>
+		<footer>
+			<h3>footer</h3>
+		</footer>
+	</div>
+)
---
+++ trying-out-layout/src/pages/index.js
@@ -0,0 +1,16 @@
+import React from "react"
+import Header from "../components/header"
+import Footer from "../components/footer"
+import Sidebar from "../components/sidebar"
+
+export default () => (
+	<div>
+		<Header>
+			<h3>Header</h3>
+		</Header>
+		
+		<Footer>
+
+		</Footer>
+	</div>
+)
